23973802d1a6520b583e6f57fe59a6b34f3fc4f1
m0in92 <moinahmed100@gmail.com>
2023-11-20 00:35:53 -0500

    added simulation results to the plot

--- django_app/views.py
+++ django_app/views.py
@@ -5,6 +5,8 @@ from bokeh.embed import components
 
 from django_app.forms import SimulationVariables
 
+import SPPy
+
 
 def index(request) -> HttpResponse:
     if request.method == "POST":
@@ -18,19 +20,44 @@ def index(request) -> HttpResponse:
 
 
 def result(request) -> HttpResponse:
-    get_simulation_inputs(request=request)
-    plot = figure()
-    plot.circle([1, 2, 3, 4, 5], [6, 7, 2, 4, 5], size=20, color="navy", alpha=0.5)
+    simulation_inputs = get_simulation_inputs(request=request)
+    sol = perform_simulation(simulation_inputs=simulation_inputs)
+    plot = figure(title='Voltage Profile', x_axis_label='t [s]', y_axis_label='V [V]')
+    plot.line(sol.t, sol.V, line_width=5)
 
     script, div = components(plot)
 
     return render(request=request, template_name='result.html', context={'script': script, 'div': div})
 
 
-def get_simulation_inputs(request):
+def get_simulation_inputs(request) -> tuple[str, str, str, str]:
     parameter_name = request.POST.get('parameter_name')
     battery_cell_model = request.POST.get('battery_cell_model')
     solver_type = request.POST.get('solver_type')
     cycler = request.POST.get('cycler')
-    print(parameter_name, battery_cell_model, solver_type, cycler)
+    return (parameter_name, battery_cell_model, solver_type, cycler)
 
+
+def perform_simulation(simulation_inputs: tuple[str, str, str, str]) -> SPPy.Solution:
+    # Operating parameters
+    I = 1.656
+    T = 298.15
+    V_min = 3
+    SOC_min = 0.1
+    SOC_LIB = 0.9
+
+    # Modelling parameters
+    SOC_init_p, SOC_init_n = 0.4956, 0.7568  # conditions in the literature source. Guo et al
+
+    # Setup battery components
+    cell = SPPy.BatteryCell.read_from_parametersets(parameter_set_name='test', SOC_init_p=SOC_init_p,
+                                                    SOC_init_n=SOC_init_n,
+                                                    temp_init=T)
+
+    # set-up cycler and solver
+    dc = SPPy.Discharge(discharge_current=I, v_min=V_min, SOC_LIB_min=SOC_min, SOC_LIB=SOC_LIB)
+    solver = SPPy.SPPySolver(b_cell=cell, N=5, isothermal=True, degradation=False, electrode_SOC_solver='poly')
+
+    # simulate
+    sol = solver.solve(cycler_instance=dc)
+    return sol
